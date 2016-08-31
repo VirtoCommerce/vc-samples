52d40d83776a43a65118b54716cb3c3a210fd782
eMazeika <em@virtoway.com>
2016-08-31 12:25:34 +0300

    MemberExtensionSampleModule: UI updated

---
+++ MemberExtensionSampleModule/Scripts/blades/supplier-detail.js
@@ -0,0 +1,22 @@
+﻿angular.module('contactExtMemberModule')
+    .controller('contactExtMemberModule.supplierDetailController', function ($scope) {
+        var blade = $scope.blade;
+        
+        if (blade.isNew) {
+            blade.title = 'New Supplier';
+            blade.currentEntity = angular.extend({
+                reviews: []
+            }, blade.currentEntity);
+
+            blade.fillDynamicProperties();
+        } else {
+            blade.subtitle = 'Supplier details';
+        }
+
+        // base function override (optional)
+        blade.customInitialize = function () {
+            if (!blade.isNew) {
+                blade.title = blade.currentEntity.name + '\'s details';
+            }
+        };
+    });
