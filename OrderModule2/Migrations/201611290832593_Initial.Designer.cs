3fdebfe79e7ceff9a0a82e0329f8dfbf3e960ff7
Tatarincev <et@virtoway.com>
2016-11-29 11:15:12 +0200

    Update order sample module to .NET 4.6.1 and update latest Nuget dependencies

--- OrderModule2/Migrations/201611290832593_Initial.Designer.cs
+++ OrderModule2/Migrations/201611290832593_Initial.Designer.cs
@@ -6,14 +6,14 @@ namespace OrderModule2.Migrations
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
-    [GeneratedCode("EntityFramework.Migrations", "6.1.1-30610")]
+    [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
     public sealed partial class Initial : IMigrationMetadata
     {
         private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201609210930161_Initial"; }
+            get { return "201611290832593_Initial"; }
         }
         
         string IMigrationMetadata.Source
