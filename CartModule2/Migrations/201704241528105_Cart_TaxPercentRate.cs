c5fb6184ab14489eb3b660866127d2d1204c5640
Artyom Androsov <aan@virtoway.com>
2017-04-25 11:03:15 +0200

    Update all modules to latest VC nuget dependencies #869

---
+++ CartModule2/Migrations/201704241528105_Cart_TaxPercentRate.cs
@@ -0,0 +1,18 @@
+namespace CartModule2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Cart_TaxPercentRate : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+            
+        }
+    }
+}
