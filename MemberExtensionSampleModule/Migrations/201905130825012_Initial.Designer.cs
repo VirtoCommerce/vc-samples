60baf393076ef61f003d2b48a042967cc8f2e41a
Evan Butler <evan.butler@icloud.com>
2019-05-13 15:33:15 +0700

    Updated MemberExtensionSample

--- MemberExtensionSampleModule/Migrations/201905130825012_Initial.Designer.cs
+++ MemberExtensionSampleModule/Migrations/201905130825012_Initial.Designer.cs
@@ -6,14 +6,14 @@ namespace MemberExtensionSampleModule.Web.Migrations
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
-    [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
+    [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
     public sealed partial class Initial : IMigrationMetadata
     {
         private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201611291318508_Initial"; }
+            get { return "201905130825012_Initial"; }
         }
         
         string IMigrationMetadata.Source
---
+++ MemberExtensionSampleModule/Migrations/201905130825012_Initial.cs
@@ -0,0 +1,81 @@
+namespace MemberExtensionSampleModule.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Contact2",
+                c => new
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    JobTitle = c.String(maxLength: 128),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Contact", t => t.Id, cascadeDelete: true)
+                .Index(t => t.Id);
+
+            // Convert all exist Contact records to Contact2
+            Sql("INSERT INTO dbo.Contact2 (Id) SELECT Id FROM dbo.Contact");
+
+            CreateTable(
+                "dbo.Supplier",
+                c => new
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    ContractNumber = c.String(maxLength: 128),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Member", t => t.Id)
+                .Index(t => t.Id);
+
+            CreateTable(
+                "dbo.SupplierReview",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Review = c.String(),
+                        SupplierId = c.String(nullable: false, maxLength: 128),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(),
+                        CreatedBy = c.String(maxLength: 64),
+                        ModifiedBy = c.String(maxLength: 64),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Supplier", t => t.SupplierId)
+                .Index(t => t.SupplierId);
+            
+            CreateTable(
+                "dbo.Organization",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        OrgType = c.Int(nullable: false),
+                        Description = c.String(maxLength: 256),
+                        BusinessCategory = c.String(maxLength: 64),
+                        OwnerId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Member", t => t.Id)
+                .Index(t => t.Id);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Supplier", "Id", "dbo.Member");
+            DropForeignKey("dbo.Contact2", "Id", "dbo.Contact");
+            DropForeignKey("dbo.SupplierReview", "SupplierId", "dbo.Supplier");
+
+            DropIndex("dbo.Supplier", new[] { "Id" });
+            DropIndex("dbo.Contact2", new[] { "Id" });
+            DropIndex("dbo.SupplierReview", new[] { "SupplierId" });
+            
+            DropTable("dbo.Supplier");
+            DropTable("dbo.Contact2");
+            DropTable("dbo.SupplierReview");
+        }
+    }
+}
