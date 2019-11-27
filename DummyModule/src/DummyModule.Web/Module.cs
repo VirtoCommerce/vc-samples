53fac75acb03a48c2b743644cae7fa0688479457
Egidijus Mazeika <em@virtoway.com>
2019-11-27 13:01:28 +0200

    Dummy module: 4. Fill DummyModule.Web project

---
+++ DummyModule/src/DummyModule.Web/Module.cs
@@ -0,0 +1,44 @@
+using System.Linq;
+using DummyModule.Core;
+using DummyModule.Data.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VirtoCommerce.Platform.Core.Modularity;
+using VirtoCommerce.Platform.Core.Security;
+using VirtoCommerce.Platform.Core.Settings;
+
+namespace DummyModule.Web
+{
+    public class Module : IModule
+    {
+        public ManifestModuleInfo ModuleInfo { get; set; }
+
+        public void Initialize(IServiceCollection serviceCollection)
+        {
+            // database initialization
+            var configuration = serviceCollection.BuildServiceProvider().GetRequiredService<IConfiguration>();
+            var connectionString = configuration.GetConnectionString("VirtoCommerce.Dummy") ?? configuration.GetConnectionString("VirtoCommerce");
+            serviceCollection.AddDbContext<DummyDbContext>(options => options.UseSqlServer(connectionString));
+        }
+
+        public void PostInitialize(IApplicationBuilder appBuilder)
+        {
+            // register settings
+            var settingsRegistrar = appBuilder.ApplicationServices.GetRequiredService<ISettingsRegistrar>();
+            settingsRegistrar.RegisterSettings(ModuleConstants.Settings.AllSettings, ModuleInfo.Id);
+
+            // register permissions
+            var permissionsProvider = appBuilder.ApplicationServices.GetRequiredService<IPermissionsRegistrar>();
+            permissionsProvider.RegisterPermissions(ModuleConstants.Security.Permissions.AllPermissions.Select(x =>
+                new Permission() { GroupName = "Dummy", Name = x }).ToArray());
+
+        }
+
+        public void Uninstall()
+        {
+            // do nothing in here
+        }
+    }
+}
---
+++ DummyModule/src/DummyModule.Web/Scripts/module.js
@@ -0,0 +1,9 @@
+﻿//Call this to register our module to main application
+var moduleName = "virtoCommerce.dummy";
+
+if (AppDependencies !== undefined) {
+    AppDependencies.push(moduleName);
+}
+
+angular.module(moduleName, [])
+    ;
